15a99fae5c61c9ad2e0abbad1abfd33fcd1bf835
Hongduoooo <e0641597@e.nus.edu>
2021-01-29 19:27:49 +0800

    Forum skeleton with Topic fetching and displaying. Need to click first button under Forum to create new post.

---
+++ app/src/main/java/com/team9/spda_team9/forum/ForumFragment.java
@@ -0,0 +1,186 @@
+package com.team9.spda_team9.forum;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.google.android.material.tabs.TabLayout;
+import com.team9.spda_team9.R;
+
+public class ForumFragment extends Fragment implements View.OnClickListener {
+
+    private ForumViewModel forumViewModel;
+    Button r1,r2,r3,r4,r5,r6;
+    Button l1,l2,l3,l4,l5,l6;
+    Button[] buttons;
+    Button[] lbuttons;
+    boolean[] selected = new boolean[6];
+    LinearLayout posts;
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        forumViewModel =
+                new ViewModelProvider(this).get(ForumViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_forum, container, false);
+        /*final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        TabLayout tabLayout = root.findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+        tabLayout.addTab(tabLayout.newTab().setText("Popular"));
+        tabLayout.addTab(tabLayout.newTab().setText("Personalized"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition()==0){
+                    for(int i =0;i<6;i++){
+
+                        buttons[i].setVisibility(View.VISIBLE);
+                        lbuttons[i].setVisibility(View.VISIBLE);
+
+                    }
+                }
+                if(tab.getPosition()==1){
+                    //NestedScrollView nsv =  root.findViewById(R.id.NSV);
+                    //nsv.setVisibility(View.VISIBLE);
+                    for(int i =0;i<6;i++){
+
+                        buttons[i].setVisibility(View.GONE);
+                        lbuttons[i].setVisibility(View.GONE);
+
+                    }
+                }
+                if(tab.getPosition()==2){
+                    for(int i =0;i<6;i++){
+                        if(selected[i] == false){
+                            buttons[i].setVisibility(View.GONE);
+                            lbuttons[i].setVisibility(View.GONE);
+                        }else{
+                            buttons[i].setVisibility(View.VISIBLE);
+                            lbuttons[i].setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                if(tab.getPosition()==0){
+                    postInvisible();
+                    for(int i =0;i<6;i++){
+
+                        buttons[i].setVisibility(View.VISIBLE);
+                        lbuttons[i].setVisibility(View.VISIBLE);
+
+                    }
+                }
+                if(tab.getPosition()==1){
+                    //NestedScrollView nsv =  root.findViewById(R.id.NSV);
+                    //nsv.setVisibility(View.VISIBLE);
+                    postInvisible();
+                    topicInvisible();
+                }
+                if(tab.getPosition()==2){
+                    postInvisible();
+                    for(int i =0;i<6;i++){
+                        if(selected[i] == false){
+                            buttons[i].setVisibility(View.GONE);
+                            lbuttons[i].setVisibility(View.GONE);
+                        }else{
+                            buttons[i].setVisibility(View.VISIBLE);
+                            lbuttons[i].setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
+
+            }
+        });
+
+        r1 = root.findViewById(R.id.r1);
+        r2 = root.findViewById(R.id.r2);
+        r3 = root.findViewById(R.id.r3);
+        r4 = root.findViewById(R.id.r4);
+        r5 = root.findViewById(R.id.r5);
+        r6 = root.findViewById(R.id.r6);
+        buttons = new Button[]{r1,r2,r3,r4,r5,r6};
+
+        l1 = root.findViewById(R.id.l1);
+        l2 = root.findViewById(R.id.l2);
+        l3 = root.findViewById(R.id.l3);
+        l4 = root.findViewById(R.id.l4);
+        l5 = root.findViewById(R.id.l5);
+        l6 = root.findViewById(R.id.l6);
+        lbuttons = new Button[]{l1,l2,l3,l4,l5,l6};
+
+        r1.setOnClickListener(this);
+        l1.setOnClickListener(this);
+        r2.setOnClickListener(this);
+        r3.setOnClickListener(this);
+        r4.setOnClickListener(this);
+        r5.setOnClickListener(this);
+        r6.setOnClickListener(this);
+
+        posts = root.findViewById(R.id.posts);
+
+
+
+        return root;
+
+    }
+
+    @Override
+    public void onClick(View v){
+        for(int i =0;i<6;i++){
+            if(buttons[i] == v){
+                if(selected[i] == false){
+                    selected[i] = true;
+                    buttons[i].setBackgroundColor(Color.RED);
+                }else{
+                    selected[i] = false;
+                    buttons[i].setBackgroundColor(Color.GREEN);
+                }
+            }
+        }
+        if(v == l1){
+            Intent intent = new Intent(getContext(), TopicDisplay.class);
+            getContext().startActivity(intent);
+
+        }
+
+    }
+    public void topicInvisible(){
+        for(int i =0;i<6;i++){
+            buttons[i].setVisibility(View.GONE);
+            lbuttons[i].setVisibility(View.GONE);
+        }
+    }
+    public void postVisible(){
+        topicInvisible();
+        posts.setVisibility(View.VISIBLE);
+    }
+    public void postInvisible(){
+        posts.setVisibility(View.GONE);
+    }
+}
---
+++ app/src/main/java/com/team9/spda_team9/forum/TopicDisplay.java
@@ -0,0 +1,98 @@
+package com.team9.spda_team9.forum;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.team9.spda_team9.R;
+
+import java.util.Map;
+
+public class TopicDisplay extends AppCompatActivity {
+    FirebaseFirestore DB;
+    private static final String TAG = TopicDisplay.class.getSimpleName();
+    LinearLayout ll;
+    boolean recreate = false;
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        if(recreate == true) recreate();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        recreate = true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic);
+
+
+        DB = FirebaseFirestore.getInstance();
+        ll = findViewById(R.id.topicContainer);
+        recreate = false;
+
+        Query findAllbyCat = DB.collection("Topic").whereEqualTo("category", "DatingAndRelationships");
+        findAllbyCat.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        Map<String,Object> map = document.getData();
+                        String title = (String)map.get("title");
+                        String time = (String)map.get("time");
+                        TextView tv1 = new TextView(TopicDisplay.this);
+                        TextView tv2 = new TextView(TopicDisplay.this);
+                        tv1.setText(title);
+                        tv2.setText(time);
+                        ll.addView(tv1);
+                        ll.addView(tv2);
+
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle("Topics");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TopicDisplay.this, TopicNew.class);
+                TopicDisplay.this.startActivity(intent);
+            }
+        });
+    }
+
+
+}
