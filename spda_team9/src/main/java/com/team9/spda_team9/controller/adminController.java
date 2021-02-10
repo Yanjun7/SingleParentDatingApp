a52382c7f1ccef3a88e372b32c878ce1c037a69c
Changwei <e0641610@u.nus.edu>
2021-02-10 14:22:39 +0800

    change suspend

--- spda_team9/src/main/java/com/team9/spda_team9/controller/adminController.java
+++ spda_team9/src/main/java/com/team9/spda_team9/controller/adminController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.team9.spda_team9.user.User;
 import com.team9.spda_team9.user.UserService;
 
 @Controller
@@ -23,10 +25,12 @@ public class adminController {
 	        return "Userdetail";
 	    }
 	 
-	 @RequestMapping(value = "/detail/{id}")
-	 public String suspenduser(@PathVariable("id") String username, Model model) throws InterruptedException, ExecutionException {
-		 model.addAttribute("user", userService.getUser(username));
-	        return "Userdetail";
+	 @RequestMapping(value = "/suspend/{id}")
+	 public String suspenduser(@PathVariable("id") String username,Model model) throws InterruptedException, ExecutionException {
+		 User user=userService.getUser(username);
+		 user.setSuspended(true);
+		 userService.updateUser(user);
+		 return "succedsuspend";
 	    }
 	
 	
